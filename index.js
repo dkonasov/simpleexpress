b6d3f98c1a85080561d76d4b666450f46e02ab79
Dmitriy Konasov <d.konasov@yandex.ru>
2015-12-26 21:32:17 +0300

    added gulp-uglify to package json

--- index.js
+++ index.js
@@ -1,26 +1,2 @@
-var express=require('express');
-var mongoose = require('mongoose');
-var bodyParser=require('body-parser');
-var db=mongoose.connection;
-db.on('error', console.error);
-db.once('open', function(){
-	
-	var app=express();
-
-	app.use(bodyParser.urlencoded({ extended: false }));
-	app.use(bodyParser.json());
-	app.set('view engine', 'jade');
-	app.set('views', __dirname + '/frontend/templates/basic');
-	app.use(express.static('dist'));
-	var router=require('./server_modules/router');
-	router.start(app);
-	var server=app.listen(3000, function(){
-		
-		console.log('Simpleexpress server started');
-
-	});
-	
-});
-
-mongoose.connect('mongodb://localhost/simpleexpress');
-
+var app=require("./server.js");
+app.start();
